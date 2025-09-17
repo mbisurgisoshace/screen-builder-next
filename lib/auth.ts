d3a07916209f4124b3c0286985c7822320099249
sohchy <sourabh.chakraborty@gmail.com>
2025-09-17 15:16:15 -0500

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import { Roles } from "@/types/global";
 
 export const checkRole = async (role: Roles) => {
+  
   const { sessionClaims } = await auth();
-  return sessionClaims?.metadata.role === role;
+
+  console.log("######## session claims ########", sessionClaims);
+  
+  return sessionClaims?.metadata?.role === role;
 };
