37621a799cfe5f78cc785f56dd85fe9c427dcacf
sohchy <sourabh.chakraborty@gmail.com>
2025-09-18 10:17:50 -0500

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,5 @@ import { Roles } from "@/types/global";
 
 export const checkRole = async (role: Roles) => {
   const { sessionClaims } = await auth();
-  console.log("###### session claims ######", sessionClaims, sessionClaims?.metadata)
   return sessionClaims?.metadata?.role === role;
 };
