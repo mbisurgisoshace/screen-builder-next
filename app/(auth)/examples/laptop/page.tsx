6e0ad9ca2a73774c5b809c460b078727cb9b759b
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2025-09-22 14:22:44 -0300

    Fix with example canvas

--- app/(auth)/examples/laptop/page.tsx
+++ app/(auth)/examples/laptop/page.tsx
@@ -1,25 +1,28 @@
 import { Room } from "@/components/Room";
 import InfiniteCanvas from "@/components/InfiniteCanvas";
+import { QuestionsProvider } from "@/components/CanvasModule/questions/QuestionsProvider";
 
 export default function LaptopExamplePage() {
   return (
     <div className="flex flex-col h-full">
       <div className="h-full">
         <Room roomId={`example-laptop`}>
-          <InfiniteCanvas
-            editable={false}
-            toolbarOptions={{
-              text: true,
-              table: false,
-              answer: false,
-              ellipse: true,
-              feature: false,
-              question: false,
-              rectangle: true,
-              interview: false,
-              card: true,
-            }}
-          />
+          <QuestionsProvider segments={[]} questions={[]}>
+            <InfiniteCanvas
+              editable={false}
+              toolbarOptions={{
+                text: true,
+                table: false,
+                answer: false,
+                ellipse: true,
+                feature: false,
+                question: false,
+                rectangle: true,
+                interview: false,
+                card: true,
+              }}
+            />
+          </QuestionsProvider>
         </Room>
       </div>
     </div>
--- app/(auth)/examples/pickup-truck/page.tsx
+++ app/(auth)/examples/pickup-truck/page.tsx
@@ -1,25 +1,28 @@
 import { Room } from "@/components/Room";
 import InfiniteCanvas from "@/components/InfiniteCanvas";
+import { QuestionsProvider } from "@/components/CanvasModule/questions/QuestionsProvider";
 
 export default function PickupTruckExamplePage() {
   return (
     <div className="flex flex-col h-full">
       <div className="h-full">
         <Room roomId={`example-pickup-truck`}>
-          <InfiniteCanvas
-            editable={false}
-            toolbarOptions={{
-              text: true,
-              table: false,
-              answer: false,
-              ellipse: true,
-              feature: false,
-              question: false,
-              rectangle: true,
-              interview: false,
-              card: false,
-            }}
-          />
+          <QuestionsProvider segments={[]} questions={[]}>
+            <InfiniteCanvas
+              editable={false}
+              toolbarOptions={{
+                text: true,
+                table: false,
+                answer: false,
+                ellipse: true,
+                feature: false,
+                question: false,
+                rectangle: true,
+                interview: false,
+                card: false,
+              }}
+            />
+          </QuestionsProvider>
         </Room>
       </div>
     </div>
